0efd0493fc3e201b7414ba3e2c157209abc6fedb
iwe2121 <13991961+iwe2121@users.noreply.github.com>
2022-01-11 15:08:25 -0500

    revise getstatusbarpid() to support dwmblocks-async, with pgrep this time.

--- dwm.c
+++ dwm.c
@@ -1015,7 +1015,7 @@ getstatusbarpid()
 				return statuspid;
 		}
 	}
-	if (!(fp = popen("pidof -o "STATUSBAR, "r")))
+	if (!(fp = popen("pgrep -o "STATUSBAR, "r")))
 		return -1;
 	fgets(buf, sizeof(buf), fp);
 	pclose(fp);
